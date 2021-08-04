04006c8b148a0899fb25ab77dc373b4f67e6dcac
Oliver <oliver.dentistry@gmail.com>
2021-08-04 21:48:12 +0200

    pagina new user añadida

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,4 +1,5 @@
-import { PermIdentity } from '@material-ui/icons'
+import { CalendarToday, EmailOutlined, PermIdentity, Phone, PublicOutlined, Publish } from '@material-ui/icons'
+import { Link } from 'react-router-dom'
 import './user.css'
 
 export default function User() {
@@ -6,7 +7,9 @@ export default function User() {
         <div className="user">
             <div className="userTitleContainer">
                 <h1 className="userTitle">Edit User</h1>
-                <button className="userAddButton">Create</button>
+                <Link to="/newUser">
+                    <button className="userAddButton">Create</button>
+                </Link>
             </div>
             <div className="userContainer">
                 <div className="userShow">
@@ -19,11 +22,64 @@ export default function User() {
                     </div>
                     <div className="userShowBottom">
                         <span className="userShowTitle">Account Details</span>
-                        <PermIdentity />
-                        <span className="userShowInfoTitle">annabeck99</span>
+                        <div className="userShowInfo">
+                            <PermIdentity className="userShowIcon" />
+                            <span className="userShowInfoTitle">annabeck99</span>
+                        </div>
+                        <div className="userShowInfo">
+                            <CalendarToday className="userShowIcon" />
+                            <span className="userShowInfoTitle">10.12.1999</span>
+                        </div>
+                        <span className="userShowTitle">Contact Details</span>
+                        <div className="userShowInfo">
+                            <Phone className="userShowIcon" />
+                            <span className="userShowInfoTitle">+1 235 145 998</span>
+                        </div>
+                        <div className="userShowInfo">
+                            <EmailOutlined className="userShowIcon" />
+                            <span className="userShowInfoTitle">anna@gmail.com</span>
+                        </div>
+                        <div className="userShowInfo">
+                            <PublicOutlined className="userShowIcon" />
+                            <span className="userShowInfoTitle">Ferrol | Spain</span>
+                        </div>
                     </div>
                 </div>
-                <div className="userUpdate"></div>
+                <div className="userUpdate">
+                    <span className="userUpdateTitle">Edit</span>
+                    <form className="userUpdateForm">
+                        <div className="userUpdateLeft">
+                            <div className="userUpdateItem">
+                                <label>Username</label>
+                                <input type="text" placeholder="annabeck99" className="userUpdateInput" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Full Name</label>
+                                <input type="text" placeholder="Oliver Reyes" className="userUpdateInput" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Email</label>
+                                <input type="text" placeholder="anna@gmail.com" className="userUpdateInput" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Phone</label>
+                                <input type="text" placeholder="+1 235 145 998" className="userUpdateInput" />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Address</label>
+                                <input type="text" placeholder="Ferrol | Spain" className="userUpdateInput" />
+                            </div>
+                        </div>
+                        <div className="userUpdateRight">
+                            <div className="userUpdateUpload">
+                                <img className="userUpdateImage" src={process.env.PUBLIC_URL + "/profile.jpg"} alt="" />
+                                <label htmlFor="file"><Publish className="userUpdateIcon" /></label>
+                                <input type="file" id="file" style={{ display: "none" }} />
+                            </div>
+                            <button className="userUpdateButton">Update</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
